a48b23461a94ac3d39c1b59b12eb08f8095f442d
Leo Dev Flux <330574408+leodevflux@users.noreply.github.com>
2026-09-21 19:36:03 -0300

    fix: force bounded workspace thumbnails

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,4 +1,5 @@
 import "./system.css";
+import "./thumbnails.css";
 import { SystemProvider } from "../../components/system/SystemProvider";
 import { SystemShell } from "../../components/system/SystemShell";
 import { ProtectedApp } from "../../components/auth/ProtectedApp";
