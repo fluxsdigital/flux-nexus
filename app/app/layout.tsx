591ec749036f6fe5dd26116ac9801160e9f580ef
Leo Dev Flux <330574408+leodevflux@users.noreply.github.com>
2026-09-21 20:17:58 -0300

    fix: print only technical report

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,5 +1,6 @@
 import "./system.css";
 import "./thumbnails.css";
+import "./report-print.css";
 import { SystemProvider } from "../../components/system/SystemProvider";
 import { SystemShell } from "../../components/system/SystemShell";
 import { ProtectedApp } from "../../components/auth/ProtectedApp";
